c0aa5b9144cee7710c41284847d54a34621cfad8
wdalton27 <wdalton27@students.stab.org>
2024-12-13 12:34:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var date = getColumn(url, 1);
+var usAverage = getColumn(url, 2);
+var Boston = getColumn(url, 3);
+var Chicago = getColumn(url, 4);
+var Cleveland = getColumn(url, 5);
+var Denver = getColumn(url, 6);
+var Houston = getColumn(url, 7);
+var LosAngeles = getColumn(url, 8);
+var Miami = getColumn(url, 9);
+var NYC = getColumn(url, 10);
+var SanFrancisco = getColumn(url, 11);
+var Seattle = getColumn(url, 12);
+
+function getPrice(City, usAverage, quote){
+ document.getElementById("output").innerHTML = "";
+ var price = [];
+ for (var i = 0; i < )
+}
+
