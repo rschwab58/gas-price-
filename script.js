6915d23ede6d711c770975ac2b4a95b7848973af
wdalton27 <wdalton27@students.stab.org>
2024-12-16 21:06:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,20 @@ var Miami = getColumn(url, 9);
 var NYC = getColumn(url, 10);
 var SanFrancisco = getColumn(url, 11);
 var Seattle = getColumn(url, 12);
+// This function is calculating the average price for each city, month, and year. 
 function getPrice(City, Month, Year){
+    console.log(City)
+    console.log(Month)
+    console.log(Year)
+
+    if(City == "City" || Month == "Month"){
+        document.getElementById("output").innerHTML = "Please enter all information.";
+        return;
+    }
 //  document.getElementById("output").innerHTML = "";
  var matchingPrices = [];
  var chosenList;
-
+// These if statements are going and finding the certain city and giving it a chosenlist
 if ("Boston" == City){
     chosenList = Boston;}  
 else if ("Chicago" == City){
@@ -38,9 +47,10 @@ else if ("San Francisco" == City){
 else if ("Seattle" == City){
     chosenList = Seattle;}
 
-
+//This loop is finding the year and month that was put in and finding those price numbers that correspond with the information
     for (var i = 0; i < date.length; i++){
-        if(date[i].includes(Year) && (Month)){
+        if(date[i].includes(Year) && (date[i].substring(0,3) == Month || date[i].substring(0,2) == Month)){
+            console.log(date[i]);
             matchingPrices.push(parseFloat(chosenList[i]));
         }
     }
@@ -48,13 +58,19 @@ else if ("Seattle" == City){
     console.log(matchingPrices);
 var total = 0;
  for (var i = 0; i < matchingPrices.length; i++){
-//
+    total += matchingPrices[i];
  }
    
 
-document.getElementById("output").innerHTML = total/matchingPrices.length;
+//This says that if that month has no prices for that year then it outputs "No Information For That Month"
+if (total == 0){
+    document.getElementById("output").innerHTML = "No Information For That Month"
+}
+else {
+    document.getElementById("output").innerHTML = total/matchingPrices.length;
+}
+
 
 }
 
-//ticketText = "<b>day:</b>" + dayInput + "<br><b>age:</b>" + ageInput + "<br><b>price:</b> $" + price;
-//document.getElementById()
+
